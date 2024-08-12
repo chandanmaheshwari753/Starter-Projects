7775dc8d78249c84ba6af71a466f3bf8fd445efd
chandanmaheshwari753 <chandanmaheshwari753@gmail.com>
2024-08-12 22:57:28 +0530

    updated

--- src/Prev-Next/prev-next.js
+++ src/Prev-Next/prev-next.js
@@ -26,22 +26,24 @@ export default function PrevNext() {
       </button>
 
       {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
+        <div className="prev-next">
+          <div className="steps">
+            <div className="numbers">
+              <div className={step >= 1 ? "active" : ""}>1</div>
+              <div className={step >= 2 ? "active" : ""}>2</div>
+              <div className={step >= 3 ? "active" : ""}>3</div>
+            </div>
 
-          <p className="message">{`Step ${step} : ${messages[step - 1]}`}</p>
+            <p className="message">{`Step ${step} : ${messages[step - 1]}`}</p>
 
-          <div className="buttons">
-            <button onClick={handleDecrement} className="btn">
-              Previous
-            </button>
-            <button onClick={handleIncrement} className="btn">
-              Next
-            </button>
+            <div className="buttons">
+              <button onClick={handleDecrement} className="btn">
+                Previous
+              </button>
+              <button onClick={handleIncrement} className="btn">
+                Next
+              </button>
+            </div>
           </div>
         </div>
       )}
